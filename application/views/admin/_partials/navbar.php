52a31b5e955bda424cef378bd679344b9442d30d
MasDeny <kickdeny@gmail.com>
2020-06-17 04:42:03 +0700

    update view navbar admin panel

--- application/views/admin/_partials/navbar.php
+++ application/views/admin/_partials/navbar.php
@@ -1,5 +1,5 @@
 <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-<div class="app-header header-shadow">
+<div class="app-header header-shadow bg-happy-fisher header-text-dark">
     <div class="app-header__logo">
         <div class="logo-src"></div>
         <div class="header__pane ml-auto">
@@ -39,26 +39,6 @@
                 </div>
                 <button class="close"></button>
             </div>
-            <ul class="header-menu nav">
-                <li class="nav-item">
-                    <a href="javascript:void(0);" class="nav-link">
-                        <i class="nav-link-icon fa fa-database"> </i>
-                        Statistics
-                    </a>
-                </li>
-                <li class="btn-group nav-item">
-                    <a href="javascript:void(0);" class="nav-link">
-                        <i class="nav-link-icon fa fa-edit"></i>
-                        Projects
-                    </a>
-                </li>
-                <li class="dropdown nav-item">
-                    <a href="javascript:void(0);" class="nav-link">
-                        <i class="nav-link-icon fa fa-cog"></i>
-                        Settings
-                    </a>
-                </li>
-            </ul>
         </div>
         <div class="app-header-right">
             <div class="header-btn-lg pr-0">
@@ -71,13 +51,12 @@
                                     <i class="fa fa-angle-down ml-2 opacity-8"></i>
                                 </a>
                                 <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
-                                    <button type="button" tabindex="0" class="dropdown-item">User
-                                        Account</button>
-                                    <button type="button" tabindex="0" class="dropdown-item">Settings</button>
-                                    <h6 tabindex="-1" class="dropdown-header">Header</h6>
-                                    <button type="button" tabindex="0" class="dropdown-item">Actions</button>
+                                    <h6 tabindex="-1" class="dropdown-header">Pengaturan Pengguna</h6>
+                                    <button type="button" tabindex="0" class="dropdown-item">Tampilkan Profil</button>
+                                    <button type="button" tabindex="0" class="dropdown-item">Perbaharui Profil</button>
+                                    <button type="button" tabindex="0" class="dropdown-item">Ganti Password</button>
                                     <div tabindex="-1" class="dropdown-divider"></div>
-                                    <button type="button" tabindex="0" class="dropdown-item">Dividers</button>
+                                    <button type="button" tabindex="0" class="dropdown-item text-danger">Keluar Aplikasi</button>
                                 </div>
                             </div>
                         </div>
@@ -88,11 +67,6 @@
                             <div class="widget-subheading">
                                 VP People Manager
                             </div>
-                        </div>
-                        <div class="widget-content-right header-user-info ml-3">
-                            <button type="button" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
-                                <i class="fa text-white fa-calendar pr-1 pl-1"></i>
-                            </button>
                         </div>
                     </div>
                 </div>
