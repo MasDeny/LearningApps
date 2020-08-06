0a77a0347fe84eb13e497ed38d1ec57b153eb184
MasDeny <kickdeny@gmail.com>
2020-08-06 23:33:43 +0700

    localip for android

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/learning/public';
+$localIP = getHostByName(getHostName());
+$config['base_url'] = 'http://'.$localIP.'/learning/public';
 
 /*
 |--------------------------------------------------------------------------
